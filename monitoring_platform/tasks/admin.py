8a1ded15cdf8f458f3cb982f6d8ea8aa2c81f7b6
MrMrMops <ShestNik08@rambler.ru>
2025-07-01 19:30:23 +0300

    ci-cd

--- monitoring_platform/tasks/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# from django_celery_beat.models import PeriodicTask, IntervalSchedule, CrontabSchedule
-
-# admin.site.register(PeriodicTask)
-# admin.site.register(IntervalSchedule)
-# admin.site.register(CrontabSchedule)
--- monitoring_platform/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import  path
-from tasks.views import RunDeleteExpiredTasksView, RunSendRemindersView
-
-
-
-urlpatterns = [
-    path('delete-expired/', RunDeleteExpiredTasksView.as_view(), name='delete-expired'),
-    path('send-reminders/', RunSendRemindersView.as_view(), name='send-reminders'),
-]
