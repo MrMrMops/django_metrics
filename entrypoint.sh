3fb2573e835487fd8f8720088ac5973f9317108a
MrMrMops <ShestNik08@rambler.ru>
2025-07-01 22:08:36 +0300

    ci-cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-echo "Applying database migrations..."
-python manage.py migrate --noinput
+# Выполняем миграции только если не переданы другие команды
+if [ "$#" -eq 0 ]; then
+    echo "Applying database migrations..."
+    python manage.py migrate --noinput
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
+    echo "Collecting static files..."
+    python manage.py collectstatic --noinput
 
-echo "Starting server..."
-exec gunicorn monitoring_platform.wsgi:application --bind 0.0.0.0:8000
+    echo "Starting server..."
+    exec gunicorn monitoring_platform.wsgi:application --bind 0.0.0.0:8000
+else
+    # Если переданы аргументы - выполняем их
+    exec "$@"
+fi
