c6667543057ede9ac0053bdeb0abec3d8d91c226
MrMrMops <Mrjopa228@rambler.ru>
2025-07-04 21:22:44 +0300

    finish

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Выполняем миграции только если не переданы другие команды
 if [ "$#" -eq 0 ]; then
     echo "Applying database migrations..."
     python manage.py migrate --noinput
@@ -11,6 +10,5 @@ if [ "$#" -eq 0 ]; then
     echo "Starting server..."
     exec gunicorn monitoring_platform.wsgi:application --bind 0.0.0.0:8000
 else
-    # Если переданы аргументы - выполняем их
     exec "$@"
 fi
